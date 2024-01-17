2fd49185b553fff8e02298f5ca4247a6f378e366
cliveportman <clive.theportman.co>
2024-01-17 08:43:41 +0000

    comments

--- src/day2/day2b.ts
+++ src/day2/day2b.ts
@@ -20,10 +20,13 @@ try {
 const lines = input.split("\r\n");
 
 const mins = lines.map((line) => {
+  // get the pulls and set initial values to 0
   const pulls = line.split(":")[1].split(";");
   let red = 0;
   let green = 0;
   let blue = 0;
+
+  // loop through the pulls and update the values if the pull is greater than the current value
   pulls.forEach((pull) => {
     const redPull = pull.match(/(\d+) red/);
     if (redPull && Number(redPull[1]) > red) red = Number(redPull[1]);
@@ -34,13 +37,13 @@ const mins = lines.map((line) => {
     const bluePull = pull.match(/(\d+) blue/);
     if (bluePull && Number(bluePull[1]) > blue) blue = Number(bluePull[1]);
   });
+  // return the minimum values
   return [red, green, blue];
 });
 
+// calculate the powers and total them for the answer
 const powers = mins.map((min) => min.reduce((acc, value) => acc * value));
 const answer = powers.reduce((acc, value) => acc + value);
 
-console.log(mins);
-console.log(powers);
 console.log(answer);
 console.timeEnd("advent2b");
