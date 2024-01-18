01131ef891858f4140636f815e6557bc30f3707c
cliveportman <clive.theportman.co>
2024-01-18 22:29:45 +0000

    feat: day 3a

--- src/day3/day3a.ts
+++ src/day3/day3a.ts
@@ -1,12 +1,12 @@
 // Read a text file
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 console.time("advent2a");
 
 // read the input file, with error handling
 let input = "";
 try {
-  input = readFileSync("./src/day3/test.txt", "utf8");
+  input = readFileSync("./src/day3/input.txt", "utf8");
 } catch (err) {
   // the TS compiler will throw an error if trying to access err.message without checking the error's type first
   if (err instanceof Error) {
@@ -19,17 +19,14 @@ try {
 // split the text file into an array of strings, line by line
 const lines = input.split("\r\n");
 
-type CharacterCoord = {
+type characterCoords = {
+  num: string;
   start: [number, number];
   end: [number, number];
-};
-
-const characterCoords: {
-  [key: string]: CharacterCoord[];
-}[] = [];
+}[];
 const specialCoords: [number, number][] = [];
 
-const numbers: string[] = [];
+const numbers: characterCoords = [];
 
 lines.forEach((line, yIndex) => {
   const characters = line.split("");
@@ -40,23 +37,79 @@ lines.forEach((line, yIndex) => {
     if (isNaN(Number(char)) && char !== ".") {
       specialCoords.push([xIndex, yIndex]);
       if (numberString !== "") {
-        numbers.push(numberString);
+        numbers.push({
+          num: numberString,
+          start: [xIndex - numberString.length, yIndex],
+          end: [xIndex - 1, yIndex],
+        });
       }
       numberString = "";
     } else if (!isNaN(Number(char))) {
-      console.log(char);
       numberString += char;
-      console.log(numberString);
+      // Need to cover line end being a number here
+      if (xIndex === characters.length - 1) {
+        numbers.push({
+          num: numberString,
+          start: [xIndex - numberString.length + 1, yIndex],
+          end: [xIndex, yIndex],
+        });
+      }
     } else {
       if (numberString !== "") {
-        numbers.push(numberString);
+        numbers.push({
+          num: numberString,
+          start: [xIndex - numberString.length, yIndex],
+          end: [xIndex - 1, yIndex],
+        });
       }
       numberString = "";
     }
   });
 });
-console.log(specialCoords);
-console.log(numbers);
+// console.log(specialCoords);
+// console.log(numbers);
 
-// console.log(answer);
+const collisions = numbers.filter((object) => {
+  // remember the start and end are arrays of [x, y]
+  const startEdges = [
+    [object.start[0] - 1, object.start[1] - 1], // top-left
+    [object.start[0], object.start[1] - 1], // top-middle
+    [object.start[0] + 1, object.start[1] - 1], // top-right
+    [object.start[0] - 1, object.start[1]], // middle-left
+    [object.start[0] - 1, object.start[1] + 1], // bottom-left
+    [object.start[0], object.start[1] + 1], // bottom-middle
+    [object.start[0] + 1, object.start[1] + 1], // bottom-right
+  ];
+  const endEdges = [
+    [object.end[0] - 1, object.end[1] - 1], // top-left
+    [object.end[0], object.end[1] - 1], // top-middle
+    [object.end[0] + 1, object.end[1] - 1], // top-right
+    [object.end[0] + 1, object.end[1]], // middle-right
+    [object.end[0] - 1, object.end[1] + 1], // bottom-left
+    [object.end[0], object.end[1] + 1], // bottom-middle
+    [object.end[0] + 1, object.end[1] + 1], // bottom-right
+  ];
+  const collision = specialCoords.some((special) => {
+    // does the start or end of the line intersect with a special character?
+    const startCollision = startEdges.some(
+      (edge) => edge[0] === special[0] && edge[1] === special[1]
+    );
+    const endCollision = endEdges.some(
+      (edge) => edge[0] === special[0] && edge[1] === special[1]
+    );
+    if (startCollision || endCollision) return true;
+  });
+  if (collision) return true;
+  return false;
+});
+//console.log(collisions);
+// writeFileSync(
+//   "./src/day3/log.txt",
+//   collisions.map((object) => Number(object.num)).join("\n")
+// );
+const numbersWithCollisions = collisions.map((object) => Number(object.num));
+
+const answer = numbersWithCollisions.reduce((a, b) => a + b);
+
+console.log(answer);
 console.timeEnd("advent2a");
